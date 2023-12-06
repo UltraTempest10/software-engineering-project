cdfc9887884047eb45c69ce4917768dd324aff8a
UltraTempest10 <104293424+UltraTempest10@users.noreply.github.com>
2023-12-07 06:00:08 +0800

    实现异常数据展示和阈值调节

--- project/backend/data_display.py
+++ project/backend/data_display.py
@@ -65,6 +65,22 @@ def get_devices():
     finally:
         close_connection(connection, cursor)
 
+# 后端接口，用于获取所有设备列表
+@app.route('/api/all', methods=['GET'])
+def get_all_devices():
+    connection, cursor = create_connection()
+
+    try:
+        query = "SELECT id,name,no FROM device"
+        cursor.execute(query)
+        devices = cursor.fetchall()
+        return jsonify(devices)
+    except Exception as e:
+        print(f"Error fetching devices: {e}")
+        return jsonify({'error': 'Internal Server Error'}), 500
+    finally:
+        close_connection(connection, cursor)
+
 # 后端接口，用于获取可用日期列表（精确到小时）
 @app.route('/api/available_dates', methods=['GET'])
 def get_available_dates():
@@ -110,25 +126,64 @@ def get_anomaly():
     connection, cursor = create_connection()
 
     try:
-        now = datetime.now().strftime("%Y-%m-%d %H")
         data = request.json
-        device_id = data.get('device_id', 'all')
-        start_date = data.get('start_date', '2023-11-11 11:11:11')
-        end_date = data.get('end_date', now)
-
-        if device_id == 'all':
-            query = "SELECT id,time,delt_x,delt_y,delt_z FROM anomaly_data WHERE time BETWEEN %s AND %s ORDER BY time DESC"
-            cursor.execute(query, (start_date, end_date))
-        else:
-            query = "SELECT id,time,delt_x,delt_y,delt_z FROM anomaly_data WHERE id = %s AND time BETWEEN %s AND %s ORDER BY time DESC"
-            cursor.execute(query, (device_id, start_date, end_date))
-        # query = "SELECT delt_x,delt_y,delt_z FROM data WHERE id = %s AND time BETWEEN %s AND %s"
-        # cursor.execute(query, (device_id, start_date, end_date))
+        start_date = data.get('start_date')
+        end_date = data.get('end_date')
+        if start_date is None:
+            start_date = "1970-01-01 00"
+        if end_date is None:
+            end_date = "2100-01-01 00"
+        query = "SELECT id,time,delt_x,delt_y,delt_z FROM anomaly_data WHERE time BETWEEN %s AND %s ORDER BY time DESC, sequence DESC"
+        cursor.execute(query, (start_date, end_date))
         device_data = cursor.fetchall()
 
         return jsonify(device_data)
     except Exception as e:
         print(f"Error fetching device data: {e}")
+        return jsonify({'error': 'Internal Server Error'}), 500
+    finally:
+        close_connection(connection, cursor)
+
+# 后端接口，用于查询阈值
+@app.route('/api/thresholds', methods=['GET'])
+def get_threshold():
+    connection, cursor = create_connection()
+
+    try:
+        query = "SELECT x, y, z FROM threshold"
+        cursor.execute(query)
+        result = cursor.fetchall()
+        if len(result) > 0:
+            for (x, y, z) in result:
+                threshold_x = x
+                threshold_y = y
+                threshold_z = z
+
+        return jsonify({'x': threshold_x, 'y': threshold_y, 'z': threshold_z})
+    except Exception as e:
+        print(f"Error fetching threshold: {e}")
+        return jsonify({'error': 'Internal Server Error'}), 500
+    finally:
+        close_connection(connection, cursor)
+
+# 后端接口，用于修改阈值
+@app.route('/api/set_thresholds', methods=['POST'])
+def set_threshold():
+    connection, cursor = create_connection()
+
+    try:
+        data = request.json
+        x = data['x']
+        y = data['y']
+        z = data['z']
+
+        query = "UPDATE threshold SET x = %s, y = %s, z = %s"
+        cursor.execute(query, (x, y, z))
+        connection.commit()
+
+        return jsonify({'success': True})
+    except Exception as e:
+        print(f"Error setting threshold: {e}")
         return jsonify({'error': 'Internal Server Error'}), 500
     finally:
         close_connection(connection, cursor)
--- project/frontend/src/views/AlarmView.vue
+++ project/frontend/src/views/AlarmView.vue
@@ -1,6 +1,13 @@
 <template>
-    <div class="intro">
-    <span class="intro-title">自动报警</span>
+  <div class="intro">
+    <div>
+      <span class="intro-title">自动报警</span>
+      <br>
+      <div class="tool-bar">
+        <a-button class="setting-button" type="primary" @click="setThresholds">更改阈值</a-button>
+        <a-button class="setting-button" type="primary" @click="setAlarm">设置提醒方式</a-button>
+      </div>
+    </div>
     <div class="block-container">
       <Feature
         :image="require('@/assets/img/monitoring.png')"
@@ -10,18 +17,290 @@
       <Feature
         :image="require('@/assets/img/alarm.png')"
         title="邮件报警"
-        description="发现异常后，通过邮件向用户发送警告，以便即使处理风险。"
+        description="发现异常后，通过邮件向用户发送警告，以便及时处理风险。"
       />
+    </div>
+  </div>
+  <div class="main">
+    <div style="text-align: left;">
+      <a-modal v-model:visible="thresholds" title="更改阈值" @ok="setThresholdsOK" @cancel="setThresholdsCancel">
+        <div class="tool-item">
+          <span class="prompt">x : </span>
+          <a-slider style="flex: 1;" v-model:value="threshold_x" :min="0" :max="36" :step="0.1" />
+          <a-input-number v-model:value="threshold_x" :min="0" :max="36" />
+        </div>
+        <div class="tool-item">
+          <span class="prompt">y : </span>
+          <a-slider style="flex: 1;" v-model:value="threshold_y" :min="0" :max="36" :step="0.1" />
+          <a-input-number v-model:value="threshold_y" :min="0" :max="36" />
+        </div>
+        <div class="tool-item">
+          <span class="prompt">z : </span>
+          <a-slider style="flex: 1;" v-model:value="threshold_z" :min="0" :max="36" :step="0.1" />
+          <a-input-number v-model:value="threshold_z" :min="0" :max="36" />
+        </div>
+      </a-modal>
+      <a-modal v-model:visible="alarm" title="设置提醒方式" @ok="setAlarmOK" @cancel="setAlarmCancel">
+        <div class="tool-item">
+          <a-checkbox v-model="mail">邮箱</a-checkbox>
+        </div>
+      </a-modal>
+    </div>
+    <div class="data-table">
+      <a-table :columns="columns" :dataSource="deviceData" />
     </div>
   </div>
 </template>
   
 <script>
+import { ref, onMounted } from 'vue';
+import { Table, Form, FormItem, Select, SelectOption, DatePicker, Button, Modal, Slider, InputNumber, Checkbox, message } from 'ant-design-vue';
 import Feature from '/src/components/IntroBlock.vue';
   
 export default {
-    components: {
-        Feature,
-    },
+  components: {
+    ATable: Table,
+    AForm: Form,
+    AFormItem: FormItem,
+    ASelect: Select,
+    ASelectOption: SelectOption,
+    ADatePicker: DatePicker,
+    AButton: Button,
+    AModal: Modal,
+    ASlider: Slider,
+    AInputNumber: InputNumber,
+    ACheckbox: Checkbox,
+    Feature,
+  },
+  setup() {
+    const devices = ref([]);
+    const deviceData = ref([]);
+    const columns = [
+      {
+        title: 'ID',
+        dataIndex: 'id',
+        key: 'id',
+        filters: [],
+        filterSearch: true,
+        onFilter: (value, record) => record.id.indexOf(value) === 0,
+      },
+      {
+        title: '时间段',
+        dataIndex: 'time',
+        key: 'time',
+      },
+      {
+        title: 'x',
+        dataIndex: 'x',
+        key: 'x',
+        sorter: (a, b) => a.x - b.x,
+      },
+      {
+        title: 'y',
+        dataIndex: 'y',
+        key: 'y',
+        sorter: (a, b) => a.y - b.y,
+      },
+      {
+        title: 'z',
+        dataIndex: 'z',
+        key: 'z',
+        sorter: (a, b) => a.z - b.z,
+      },
+    ];
+
+    const getDevices = async () => {
+      const response = await fetch(`http://127.0.0.1:5000/api/all`);
+      const data = await response.json();
+      devices.value = data.map(item => {
+        return {
+          text: item.join(' '),
+          value: item[0]
+        };
+      });
+      console.log('devices', devices.value);
+      columns[0].filters = devices.value;
+    };
+    
+    const getAnomaly = async () => {
+      try {
+        const response = await fetch('http://127.0.0.1:5000/api/anomaly', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            'start_date': '1970-01-01 00',
+            'end_date': '2100-01-01 00',
+          }),
+        });
+
+        const fetchedData = await response.json();
+        console.log(fetchedData);
+
+        console.log('Received data from the server:', fetchedData);
+
+        if (Array.isArray(fetchedData)) {
+          // Update deviceData with the fetched data
+          deviceData.value = fetchedData.map(item => {
+            const date = new Date(item[1]);
+            const formattedDate = date.toLocaleString('zh-CN', { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit' });
+            const dateBeforeTenMinutes = new Date(date.getTime() - 10 * 60 * 1000);
+            const formattedDateBeforeTenMinutes = dateBeforeTenMinutes.toLocaleString('zh-CN', { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit' });
+            const timePeriod = formattedDateBeforeTenMinutes + ' - ' + formattedDate;
+
+            return {
+              id: item[0],
+              time: timePeriod,
+              x: item[2],
+              y: item[3],
+              z: item[4],
+            };
+          }).filter(item => {
+            return Math.abs(item.x) > threshold_x.value || Math.abs(item.y) > threshold_y.value || Math.abs(item.z) > threshold_z.value;
+          });
+          console.log(deviceData.value)
+        } else {
+          console.error('Received non-array data from the server:', fetchedData);
+          // Handle the error or return a default value
+          return [];
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        // Handle the error or return a default value
+        return [];
+      }
+    };
+
+    const thresholds = ref(false);
+    const alarm = ref(false);
+    const threshold_x = ref(0);
+    const threshold_y = ref(0);
+    const threshold_z = ref(0);
+
+    const getThresholds = async () => {
+      try {
+        const response = await fetch(`http://127.0.0.1:5000/api/thresholds`);
+        const data = await response.json();
+        threshold_x.value = data['x'];
+        threshold_y.value = data['y'];
+        threshold_z.value = data['z'];
+        console.log('Received data from the server:', data);
+        console.log('thresholds', threshold_x.value, threshold_y.value, threshold_z.value);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        // Handle the error or return a default value
+        return [];
+      }
+    };
+
+    const setThresholds = () => {
+      thresholds.value = true;
+    };
+
+    const setThresholdsOK = async () => {
+      try {
+        const response = await fetch('http://127.0.0.1:5000/api/set_thresholds', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            'x': threshold_x.value,
+            'y': threshold_y.value,
+            'z': threshold_z.value,
+          }),
+        });
+        getAnomaly();
+        message.success('设置成功');
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        // Handle the error or return a default value
+        message.error('设置失败, 请检查网络连接或联系网站管理员');
+        return [];
+      }
+      thresholds.value = false;
+    };
+
+    const setThresholdsCancel = () => {
+      thresholds.value = false;
+      getThresholds();
+    };
+
+    const mail = ref(false);
+
+    const setAlarm = () => {
+      alarm.value = true;
+    };
+
+    const setAlarmOK = async () => {
+      try {
+        
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        // Handle the error or return a default value
+        message.error('设置失败, 请检查网络连接或联系网站管理员');
+        return [];
+      }
+      alarm.value = false;
+    };
+
+    const setAlarmCancel = () => {
+      alarm.value = false;
+    };
+
+    onMounted(() => {
+      getAnomaly();
+      getDevices();
+      getThresholds();
+    });
+
+    return {
+      devices,
+      deviceData,
+      columns,
+      thresholds,
+      alarm,
+      threshold_x,
+      threshold_y,
+      threshold_z,
+      getAnomaly,
+      getDevices,
+      getThresholds,
+      setThresholds,
+      setThresholdsOK,
+      setThresholdsCancel,
+      setAlarm,
+      setAlarmOK,
+      setAlarmCancel,
+    };
+  },
 };
 </script>
+
+<style>
+.choices {
+  size: 20px;
+  margin-top: 50px;
+}
+
+.data-table {
+  margin: 20px;
+  border-radius: 10px;
+  box-shadow: 0px 0px 8px rgba(10, 10, 10, 0.3);
+}
+
+.tool-bar {
+  display: flex;
+  margin-top: 36px;
+}
+
+.setting-button {
+  margin-left: 28px;
+}
+
+.prompt {
+  font-size: medium;
+  margin-right: 20px;
+}
+</style>
